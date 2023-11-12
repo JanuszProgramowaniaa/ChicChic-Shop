4f22cdb5a9dce0ffe04f0dcb6590befa7074ea79
Damian Wasiak <damianwasiak49@gmail.com>
2023-11-12 23:45:57 +0100

    mainPage display products

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,14 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProductRepository;
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+
+        $paginatedProducts = $productRepository->findAllPaginated(2, 6);
+        $paginatedProducts2 = $productRepository->findAllPaginated(1, 6);
+
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'items' => $paginatedProducts,
+            'items2' => $paginatedProducts2
         ]);
     }
 }
