11ca32daede6d1dddd60a87b3ac0f3996714d5dd
Damian Wasiak <damianwasiak49@gmail.com>
2023-11-19 23:21:55 +0100

    configure scss webpack

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -6,12 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AboutAsController extends AbstractController
+class AboutController extends AbstractController
 {
-    #[Route('/about/as', name: 'app_about_as')]
+    #[Route('/about', name: 'app_about_index')]
     public function index(): Response
     {
-        return $this->render('about_as/index.html.twig', [
+        return $this->render('about/index.html.twig', [
         ]);
     }
 }
